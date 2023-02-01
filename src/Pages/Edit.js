4b916a83a57b339cdacbcaedf60cd40391a5259d
dilrubamajumder <dilrubamajumder@pursuit.org>
2023-01-31 22:43:43 -0500

    Css updated Again

--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -1,11 +1,37 @@
 import React from "react";
 import EditSnackForm from "../Components/EditSnackForm";
-
+import { Link, useParams } from "react-router-dom";
+import "./edit.css"
 export default function Edit() {
+  const {id} = useParams()
   return (
     <div>
-      <h4>Edit a Snack</h4>
+      <div>
+      <img
+          className="editimg"
+          src="https://i0.wp.com/i.pinimg.com/originals/93/a2/d7/93a2d7556d1d4fc891bfa32b753f01fd.gif"
+          alt="editimg"
+        />
+      </div> 
+      <div className="editform">
+       {/* <div>
+        <img
+          className="new-img"
+          src="https://i0.wp.com/i.pinimg.com/originals/93/a2/d7/93a2d7556d1d4fc891bfa32b753f01fd.gif"
+          alt="homeimg"
+          width={200}
+        />
+       </div> */}
+       <div>
+        <h4>Edit a Snack</h4>
       <EditSnackForm />
+      <div className="back-btn">
+        <Link to={`/snacks/${id}`}>Back</Link>
+      </div>
+       </div>
+           
+      </div>
+   
     </div>
   );
 }
